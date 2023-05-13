27b407b05f2d0689a39e921b7393ad5efdccef48
Ailadir <117507118+Ailadir@users.noreply.github.com>
2023-05-14 00:51:25 +0300

    Add script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 const input = document.querySelectorAll('.signup__form-input');
+
+input.forEach((item) =>
+  item.addEventListener('keydown', (event) => {
+    item.classList.add('signup__form-input-active');
+  })
+);
+
+// input.forEach((item) =>
+//   item.addEventListener('keyup', (event) => {
+//     item.classList.remove('-active');
+//   })
+// );

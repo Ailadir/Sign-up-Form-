7dae08c65be3e927b594e7da5d9ef663d733012f
Ailadir <117507118+Ailadir@users.noreply.github.com>
2023-05-15 19:59:21 +0300

    Divide scss by files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
 const input = document.querySelectorAll('.signup__form-input');
+const firstPassword = document.querySelector('.first_password');
+const secondPassword = document.querySelector('.second_password');
 
 input.forEach((item) =>
-  item.addEventListener('keydown', (event) => {
+  item.addEventListener('focusout', (event) => {
     item.classList.add('signup__form-input-active');
   })
 );
 
-input.forEach((item) =>
-  item.addEventListener('keyup', (event) => {
-    item.classList.remove('-active');
-  })
-);
+// input.forEach((item) =>
+//   item.addEventListener('keyup', (event) => {
+//     item.classList.remove('-active');
+//   })
+// );
+
+// Check passwords
+secondPassword.addEventListener('focusout', (event) => {
+  if (firstPassword.textContent !== secondPassword.textContent) {
+    secondPassword.attributes.removeNamedItem(':valid');
+    secondPassword.setCustomValidity('Hey, its not working');
+  }
+});

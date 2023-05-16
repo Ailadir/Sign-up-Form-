35a7e455cb44161170f30c76286e0fec1d89f3d9
Ailadir <117507118+Ailadir@users.noreply.github.com>
2023-05-16 12:51:21 +0300

    Add UI tweaks Add new page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 const input = document.querySelectorAll('.signup__form-input');
 const firstPassword = document.querySelector('.first_password');
 const secondPassword = document.querySelector('.second_password');
+const button = document.querySelector('.btn');
+const registrationPage = document.getElementById('home');
+const registredPage = document.getElementById('success');
+
+let checker = (arr) => arr.every(Boolean);
 
 input.forEach((item) =>
   item.addEventListener('focusout', (event) => {
@@ -8,16 +13,25 @@ input.forEach((item) =>
   })
 );
 
-// input.forEach((item) =>
-//   item.addEventListener('keyup', (event) => {
-//     item.classList.remove('-active');
-//   })
-// );
-
 // Check passwords
-secondPassword.addEventListener('focusout', (event) => {
-  if (firstPassword.textContent !== secondPassword.textContent) {
-    secondPassword.attributes.removeNamedItem(':valid');
-    secondPassword.setCustomValidity('Hey, its not working');
+secondPassword.addEventListener('keyup', (event) => {
+  if (firstPassword.value !== secondPassword.value) {
+    secondPassword.setCustomValidity('Error: Passwords do not match, please ensure both entries are identical.');
+  } else {
+    secondPassword.setCustomValidity('');
+  }
+});
+
+// Show another page when all input field are valid and person click on create account button
+
+button.addEventListener('click', (event) => {
+  const checkArray = [];
+  console.log(checkArray);
+  input.forEach((item) => {
+    checkArray.push(item.validity.valid);
+  });
+  if (checker(checkArray)) {
+    registredPage.style.opacity = 1;
+    registrationPage.style.display = 'none';
   }
 });

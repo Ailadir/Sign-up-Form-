9210bcc2a45940301461838e5cb3af0b17236b22
Federico Seitún <fseitun@gmail.com>
2023-05-17 09:51:29 -0300

    test commit to show PR

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,31 @@
-const input = document.querySelectorAll('.signup__form-input');
-const firstPassword = document.querySelector('.first_password');
-const secondPassword = document.querySelector('.second_password');
-const button = document.querySelector('.btn');
-const registrationPage = document.getElementById('home');
-const registredPage = document.getElementById('success');
+const input = document.querySelectorAll(".signup__form-input");
+const firstPassword = document.querySelector(".first_password");
+const secondPassword = document.querySelector(".second_password");
+const button = document.querySelector(".btn");
+const registrationPage = document.getElementById("home");
+const registredPage = document.getElementById("success");
 const checker = (arr) => arr.every(Boolean);
 
 input.forEach((item) =>
-  item.addEventListener('focusout', (event) => {
-    item.classList.add('signup__form-input-active');
+  item.addEventListener("focusout", (event) => {
+    item.classList.add("signup__form-input-active");
   })
 );
 
 // Check passwords
-secondPassword.addEventListener('keyup', (event) => {
+secondPassword.addEventListener("keyup", (event) => {
   if (firstPassword.value !== secondPassword.value) {
-    secondPassword.setCustomValidity('Error: Passwords do not match, please ensure both entries are identical.');
+    secondPassword.setCustomValidity(
+      "Error: Passwords do not match, please ensure both entries are identical."
+    );
   } else {
-    secondPassword.setCustomValidity('');
+    secondPassword.setCustomValidity("");
   }
 });
 
 // Show another page when all input field are valid and person click on create account button
 
-button.addEventListener('click', (event) => {
+button.addEventListener("click", (event) => {
   const checkArray = [];
   console.log(checkArray);
   input.forEach((item) => {
@@ -31,6 +33,6 @@ button.addEventListener('click', (event) => {
   });
   if (checker(checkArray)) {
     registredPage.style.opacity = 1;
-    registrationPage.style.display = 'none';
+    registrationPage.style.display = "none";
   }
 });
